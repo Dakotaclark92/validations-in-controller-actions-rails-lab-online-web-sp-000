50946e3fc0127c7e06a961ca3617932519e68d5b
Dakota Clark <dakotaclark92@yahoo.com>
2019-10-06 02:06:51 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,6 +2,3 @@ class Author < ActiveRecord::Base
   validates :name, presence: true
   validates :email, uniqueness: true
 end
-
-Author.create(name: "John Doe").valid?
-Author.create(name: nil).valid?
