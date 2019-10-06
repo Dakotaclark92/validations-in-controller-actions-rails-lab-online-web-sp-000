57bd66a785b57875f953e6055d04633bdddad57e
Dakota Clark <dakotaclark92@yahoo.com>
2019-10-06 02:13:38 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 100 }
-  validates :category, inclution: { in %
-end
+  validates :category, inclution: { in: %w(Fiction Non-Fiction) }
+  end
